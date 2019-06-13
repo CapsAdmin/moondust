7db2116f311f4853cb2842179949a1913ee03e12
Elias Hogstvedt <eliashogstvedt@gmail.com>
2019-06-14 00:20:33 +0200

    improve tests

--- tests/init.lua
+++ tests/init.lua
@@ -1,15 +1,7 @@
-print("running tests..")
-
-if jit.os ~= "Windows" then
-	loadfile("tests/comparisons.lua")()
-end
-loadfile("tests/execute.lua")()
-
-print("tests ran!")
 
 print("running examples..")
 
-loadfile("examples/dump_gas.lua")()
+--loadfile("examples/dump_gas.lua")()
 loadfile("examples/hello_world.lua")()
 loadfile("examples/hello_world_alt.lua")()
 loadfile("examples/labels.lua")()
@@ -18,3 +10,12 @@ loadfile("examples/vec3_length.lua")()
 print("examples ran!")
 
 print("test complete!")
+
+print("running tests..")
+
+if jit.os ~= "Windows" then
+	loadfile("tests/comparisons.lua")()
+end
+loadfile("tests/execute.lua")()
+
+print("tests ran!")
