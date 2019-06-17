066695954b8f9d8e55fd3d6f0a40ec85b6494f27
Elias Hogstvedt <eliashogstvedt@gmail.com>
2019-06-17 08:59:01 +0200

    cleanup tests

--- tests/execute.lua
+++ tests/execute.lua
@@ -6,6 +6,13 @@ local x86_64 = require("moondust.x86_64")
 local asm = require("moondust.assembler")
 local r = asm.reg
 
+local return_value = r.rax
+local arg1, arg2, arg3, arg4, arg5, arg6 = r.rdi, r.rsi, r.rdx, r.rcx, r.r8, r.r9
+local temp1, temp2 = r.r10, r.r11
+local stack_pointer = r.rsp
+local frame_pointer = r.rbp
+local global_pointer = r.r15
+
 local function check(res, op, target, msg)
     if op == "==" then
         if res == target then
@@ -17,118 +24,6 @@ local function check(res, op, target, msg)
     else
         error("unhanled op " .. op, 2)
     end
-end
-
-do -- 2+5 memory
-    local a = asm.assembler()
-    local res = ffi.new("int[1]", 2)
-
-    do
-        a:mov(r.rax, 3)
-        a:add(r(asm.object_to_address(res)), r.rax)
-
-        a:ret()
-    end
-
-    local mcode = a:compile()
-    local func = ffi.cast("int (*)()", mcode)
-    check(func(), "==", 3, "rax should be TARGET")
-    check(res[0], "==", 5, "memory destination should be TARGET")
-end
-
-do -- 2+5 registry
-    local a = asm.assembler()
-    do
-        a:mov(r.rax, 3)
-        a:add(r.rax, 2)
-
-        a:ret()
-    end
-
-    local mcode = a:compile()
-    local func = ffi.cast("int (*)()", mcode)
-
-    local res = func()
-    check(res, "==", 5, "rax should be TARGET")
-end
-
-do -- 2+5 memory with offset
-    local a = asm.assembler()
-    local res = ffi.new("int[1]", 2)
-
-    do
-        a:mov(r.rdx, asm.object_to_address(res) - 1024)
-
-        a:mov(r.rax, 3)
-        a:add(r.rdx + 1024, r.rax)
-
-        a:ret()
-    end
-
-    local mcode = a:compile()
-    local func = ffi.cast("void (*)()", mcode)
-    func()
-    check(res[0], "==", 5, "int should be TARGET")
-end
-
-
-do -- 2+5 double
-    local a = asm.assembler()
-
-    local res = ffi.new("double[1]", 0)
-    do
-        a:movsd(r.xmm0, r(asm.object_to_address(ffi.new("double[1]", 3))))
-        a:addsd(r.xmm0, r(asm.object_to_address(ffi.new("double[1]", 2))))
-        a:movsd(r(asm.object_to_address(res)), r.xmm0)
-
-        a:ret()
-    end
-
-    local mcode = a:compile()
-
-    local func = ffi.cast("int (*)()", mcode)
-
-    func()
-
-    check(res[0], "==", 5, "rax should be TARGET")
-end
-
-
-do -- 3*2 double
-    local a = asm.assembler()
-
-    local res = ffi.new("double[1]", 0)
-    do
-        a:movsd(r.xmm0, r(asm.object_to_address(ffi.new("double[1]", 3))))
-        a:mulsd(r.xmm0, r(asm.object_to_address(ffi.new("double[1]", 2))))
-        a:movsd(r(asm.object_to_address(res)), r.xmm0)
-
-        a:ret()
-    end
-
-    local mcode = a:compile()
-
-    local func = ffi.cast("int (*)()", mcode)
-
-    func()
-
-    check(res[0], "==", 6, "rax should be TARGET")
-end
-
-do -- left shift
-    local a = asm.assembler()
-    do
-        a:mov(r.rax, 0xa)
-        a:shl(r.rax, 2)
-        a:ret()
-    end
-
-    local mcode = a:compile()
-
-    local func = ffi.cast("uint32_t (*)()", mcode)
-
-    local res = func()
-    check(res, "==", 0x28, "rax should be TARGET")
 end
 
 local function generic_output(output, assemble)
@@ -174,7 +69,6 @@ check(math.sqrt(x*x + y*y + z*z), "==", generic_output(ffi.new("double[1]"), fun
     a:ret()
 end, "sqrt(x*x + y*y + z*z) should be TARGET"))
 
-
 check(1337ull, "==", generic_return(function(a)
     a:push(ffi.new("uint32_t", 1337))
     a:pop(r.rax)
@@ -190,6 +84,66 @@ check(1337ull, "==", generic_return(function(a)
     a:ret()
 end), "should be TARGET")
 
-local function address_of(name)
-    ffi.cdef("void *" .. name) return asm.object_to_address(ffi.C[name])
+check(0x28, "==", generic_return(function(a)
+    a:mov(r.rax, 0xa)
+    a:shl(r.rax, 2)
+    a:ret()
+end), "rax should be TARGET")
+
+check(6, "==", generic_output(ffi.new("double[1]"), function(a, output)
+    a:movsd(r.xmm0, r(asm.object_to_address(ffi.new("double[1]", 3))))
+    a:mulsd(r.xmm0, r(asm.object_to_address(ffi.new("double[1]", 2))))
+    a:movsd(r(asm.object_to_address(output)), r.xmm0)
+
+    a:ret()
+end), "expected TARGET")
+
+check(5, "==", generic_output(ffi.new("double[1]"), function(a, output)
+    a:movsd(r.xmm0, r(asm.object_to_address(ffi.new("double[1]", 3))))
+    a:addsd(r.xmm0, r(asm.object_to_address(ffi.new("double[1]", 2))))
+    a:movsd(r(asm.object_to_address(output)), r.xmm0)
+
+    a:ret()
+end), "expected TARGET")
+
+check(5, "==", generic_output(ffi.new("int[1]", 2), function(a, output)
+    a:mov(r.rdx, asm.object_to_address(output) - 1024)
+
+    a:mov(r.rax, 3)
+    a:add(r.rdx + 1024, r.rax)
+
+    a:ret()
+end), "expected TARGET")
+
+check(5, "==", generic_return(function(a, output)
+    a:mov(return_value, 3)
+    a:add(return_value, 2)
+
+    a:ret()
+end), "expected TARGET")
+
+check(3, "==", generic_return(function(a, output)
+    a:mov(return_value, 3)
+    a:push(return_value)
+    a:pop(arg1)
+    a:mov(return_value, arg1)
+    a:ret()
+end), "expected TARGET")
+
+
+do -- 2+5 memory
+    local a = asm.assembler()
+    local res = ffi.new("int[1]", 2)
+
+    do
+        a:mov(return_value, 3)
+        a:add(r(asm.object_to_address(res)), return_value)
+
+        a:ret()
+    end
+
+    local mcode = a:compile()
+    local func = ffi.cast("int (*)()", mcode)
+    check(func(), "==", 3, "rax should be TARGET")
+    check(res[0], "==", 5, "memory destination should be TARGET")
 end
